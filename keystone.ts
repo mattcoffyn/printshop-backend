583008917110a69a761d96ff35af0b2314e020e7
Matt Coffyn <matt.coffyn@gmail.com>
2022-02-02 10:39:33 +0000

    Refactor schema list

--- keystone.ts
+++ keystone.ts
@@ -1,18 +1,7 @@
 import 'dotenv/config';
 import { config } from '@keystone-6/core';
 import { withAuth, session } from './auth';
-import { User } from './schemas/User';
-import { Role } from './schemas/Role';
-import { Product } from './schemas/Product';
-import { ProductImage } from './schemas/ProductImage';
-import { ProcessProduct } from './schemas/ProcessProduct';
-import { Order } from './schemas/Order';
-import { CartProcessProduct } from './schemas/CartProcessProduct';
-import { FilmColour } from './schemas/FilmColour';
-import { FilmType } from './schemas/FilmType';
-import { ScanResolution } from './schemas/ScanResolution';
-import { CartItem } from './schemas/CartItem';
-import { OrderStatus } from './schemas/OrderStatus';
+import { lists } from './schemas/schema';
 import { insertSeedData } from './seed-data';
 import { extendGraphqlSchema } from './mutations';
 
@@ -21,21 +10,6 @@ const dashboardURL = process.env.DASHBOARD_URL || 'http://localhost:4000';
 const deployURL =
   process.env.DEPLOY_URL || 'https://printshop-dashboard.vercel.app';
 const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5000';
-
-const lists = {
-  User,
-  Role,
-  Product,
-  ProductImage,
-  ProcessProduct,
-  CartProcessProduct,
-  FilmColour,
-  FilmType,
-  ScanResolution,
-  CartItem,
-  OrderStatus,
-  Order,
-};
 
 export default withAuth(
   config({
@@ -63,10 +37,7 @@ export default withAuth(
     lists,
     extendGraphqlSchema,
     ui: {
-      isAccessAllowed: ({ session }) => {
-        console.log(session);
-        return !!session;
-      },
+      isAccessAllowed: ({ session }) => !!session,
     },
     session: session,
   })
---
+++ schemas/schema.ts
@@ -0,0 +1,27 @@
+import { User } from './User';
+import { Role } from './Role';
+import { Product } from './Product';
+import { ProductImage } from './ProductImage';
+import { ProcessProduct } from './ProcessProduct';
+import { Order } from './Order';
+import { CartProcessProduct } from './CartProcessProduct';
+import { FilmColour } from './FilmColour';
+import { FilmType } from './FilmType';
+import { ScanResolution } from './ScanResolution';
+import { CartItem } from './CartItem';
+import { OrderStatus } from './OrderStatus';
+
+export const lists = {
+  User,
+  Role,
+  Product,
+  ProductImage,
+  ProcessProduct,
+  CartProcessProduct,
+  FilmColour,
+  FilmType,
+  ScanResolution,
+  CartItem,
+  OrderStatus,
+  Order,
+};
